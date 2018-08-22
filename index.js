1ea3fbcff8cee32f4d7af0c14b1c7fa1f71e4248
r3qu3stt1me0ut <r3qu3stt1me0ut@gmail.com>
2018-08-22 10:07:16 +0800

    add Button&&Input styles

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 require("@test");
+setBodyFontSize();
+
+function setBodyFontSize() {
+  document.documentElement.style.fontSize = (window.innerWidth / 100) + 'px';
+}
+window.onresize = setBodyFontSize;
